757cd2fa536c673f2c9293adeed4c1807c1dc239
Han Xu <hanxu27@gmail.com>
2019-06-01 19:50:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here
+const var1 = 2;
+const var2 = 31;
+const multiply = var1 * var2;
+const random = Math.ceil(Math.random() * 10 );
+const mod =  9 % 5
+const max = Math.max(20)
