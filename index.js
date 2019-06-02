636c746eafd227ec3edfb91be1691ab03774aa7a
Andrew Allen <asallen67@gmail.com>
2019-06-02 15:48:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here
+const var1 = 1
+const var2 = 62
+const multiply = var1 * var2
+const random = Math.floor(Math.random()*10) + 1
+const mod = 4 % 5
+const max = Math.max(1, 20, 3, 4, 5)
